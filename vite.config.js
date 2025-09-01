c8ceeb04c5ab7e70e08f7bc9a42573985a171f26
Aman-0402 <aman08.stars@gmail.com>
2025-09-01 15:21:54 +0530

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Replace with your GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: "/Personal-Portfolio/", // 👈 must match your repo name
+  base: "/Personal-Portfolio/", // 👈 your repo name
 });
